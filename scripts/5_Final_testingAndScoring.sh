1166fc0d8b296351dd232432c5ae4fb393ac5298
Bernard Potocki <bernard.potocki@imanel.org>
2014-05-30 20:28:40 +0200

    Fix remaining problems

--- scripts/5_Final_testingAndScoring.sh
+++ scripts/5_Final_testingAndScoring.sh
@@ -11,23 +11,23 @@
 	echo "		done, see log/ComputeTest_tnorm.log"
 
 # 7. Compute test for scores normalization (zt-norm)
-#	echo "Compute ZT-norm scores"
-#	bin/ComputeTest --config cfg/ComputeTestZTnorm.cfg &> log/ComputeTestztnorm.log
-#	echo "		done, see log/ComputeTest_ztnorm.log"
+	echo "Compute ZT-norm scores"
+	bin/ComputeTest --config cfg/ComputeTestZTnorm.cfg &> log/ComputeTestztnorm.log
+	echo "		done, see log/ComputeTest_ztnorm.log"
 
 # 8. Score Normalization: Z-norm
-#	echo "Apply Z-norm"
-#	bin/ComputeNorm --config cfg/ComputeNorm_znorm.cfg &> log/ComputeNorm_znorm.log
-#	echo "		done, see log/ComputeNorm_znorm.log"
+	echo "Apply Z-norm"
+	bin/ComputeNorm --config cfg/ComputeNorm_znorm.cfg &> log/ComputeNorm_znorm.log
+	echo "		done, see log/ComputeNorm_znorm.log"
 
 # 9. Score Normalization: T-norm
-#	echo "Apply T-norm"
-#	bin/ComputeNorm --config cfg/ComputeNorm_tnorm.cfg &> log/ComputeNorm_tnorm.log
-#	echo "		done, see log/ComputeNorm_tnorm.log"
+	echo "Apply T-norm"
+	bin/ComputeNorm --config cfg/ComputeNorm_tnorm.cfg &> log/ComputeNorm_tnorm.log
+	echo "		done, see log/ComputeNorm_tnorm.log"
 
 # Trials:
-	bin/ComputeNorm --tnormNistFile res/target-seg_gmm.res --testNistFile res/imp-seg_gmm.res --outputFileBaseName swallow --normType tnorm
-	echo "		done, see outputfile"
+#	bin/ComputeNorm --tnormNistFile res/target-seg_gmm.res --testNistFile res/imp-seg_gmm.res --outputFileBaseName swallow --normType tnorm
+#	echo "		done, see outputfile"
 
 
 # 10. Score Normalization: ZT-norm
@@ -36,9 +36,9 @@
 #	note that in this configuration, scores after t-norm are also provided as output
 #	in a symetric manner, TZ-norm return scores after Z-norm and TZ-norm
 #
-#	echo "Apply ZT-norm"
-#	bin/ComputeNorm --config cfg/ComputeNorm_ztnorm.cfg &> log/ComputeNorm_ztnorm.log
-#	echo "		done, see log/ComputeNorm_ztnorm.log"
+	echo "Apply ZT-norm"
+	bin/ComputeNorm --config cfg/ComputeNorm_ztnorm.cfg &> log/ComputeNorm_ztnorm.log
+	echo "		done, see log/ComputeNorm_ztnorm.log"
 
 	
 	
